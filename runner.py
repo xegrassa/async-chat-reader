22b2c5cd5ee2cf03964b0f7752a547c92fbd3328
Egrassa <bacek90@mail.ru>
2021-06-20 18:11:43 +0500

    Save messages history to file.

--- runner.py
+++ runner.py
@@ -1,11 +1,21 @@
 import asyncio
+import datetime
+
+import aiofiles
 
 
 async def read_chat():
     while True:
         reader, _ = await asyncio.open_connection('minechat.dvmn.org', 5000)
         data = await reader.read(100)
-        print(f'{data.decode()!r}')
+
+        message_text = data.decode()
+        message_time = datetime.datetime.now().strftime('[%d.%m.%y %H:%M]')
+        message = message_time + ' ' + message_text
+
+        async with aiofiles.open('chatlog.txt', mode='a', encoding='utf-8') as f:
+            await f.write(message)
+        print(f'{message}')
 
 
 if __name__ == '__main__':
