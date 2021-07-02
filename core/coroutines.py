5a5b1a0ef3d08d10ddadd1789155dfb4c6ffe04b
egrassa <bacek90@mail.ru>
2021-07-02 09:58:20 +0500

    Add auto close connection.

--- core/coroutines.py
+++ core/coroutines.py
@@ -9,29 +9,25 @@ from core.my_logging import configure_logging
 configure_logging(__name__)
 
 
-async def register_chat(host, port, name, token_path):
+async def register_chat(host: str, port: str, name: str, token_path: str):
     reader, writer = await asyncio.open_connection(host, port)
+    try:
+        await read_message(reader)
+        await submit_message(writer)
+        await read_message(reader)
+        await submit_message(writer, name)
+        message = await read_message(reader)
+    finally:
+        writer.close()
+        await writer.wait_closed()
 
-    await read_message(reader)
-    await submit_message(writer)
-    await read_message(reader)
-    await submit_message(writer, name)
-    message = await read_message(reader)
     _, token = json.loads(message).values()
-
     async with aiofiles.open(token_path, 'w', encoding='utf-8') as f:
         await f.write(f'token = {token}')
     return token
 
 
-async def read_message(reader):
-    logger = logging.getLogger(__name__)
-    data = await reader.readline()
-    logger.debug(data.decode())
-    return data
-
-
-async def authorise(host, port, token):
+async def authorise(host: str, port: str, token: str):
     logger = logging.getLogger(__name__)
     reader, writer = await asyncio.open_connection(host, port)
     try:
@@ -42,14 +38,21 @@ async def authorise(host, port, token):
         if json.loads(data) is None:
             print('Неизвестный токен. Проверьте его или зарегистрируйте заново.')
             return
-        logger.debug(data.decode())
+        logger.debug(data)
         await read_message(reader)
     finally:
         writer.close()
         await writer.wait_closed()
 
 
-async def submit_message(writer, message: str = ''):
+async def read_message(reader: asyncio.StreamReader) -> str:
+    logger = logging.getLogger(__name__)
+    response = await reader.readline()
+    logger.debug(response.decode())
+    return response.decode()
+
+
+async def submit_message(writer: asyncio.StreamWriter, message: str = ''):
     logger = logging.getLogger(__name__)
     send_message = message.replace('\n', '')
     logger.debug(send_message)
